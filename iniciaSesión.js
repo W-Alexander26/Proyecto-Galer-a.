392086a87b079b9c51e322fbe21bfcd66bf155ed
W-Alexander <alexanderaguilar726@gmail.com>
2024-05-07 15:53:09 -0600

    Unión de los datos del usuario con el local storage en página de inicio de sesión

--- iniciaSesión.js
+++ iniciaSesión.js
@@ -1,8 +1,38 @@
 let correo=document.getElementById("correos");
+
 let contraseña=document.getElementById("contraseñas");
 
+let correoValido= /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
 function IniciarSesión() {
-  localStorage.setItem('usuario2', correo.value + " " + contraseña.value);
-  localStorage.getItem('usuario2');
+
+
+  let valor=localStorage.getItem('correo');
+
+  let correosAlmacenados=valor.split(",")
   
+  for (let index = 0; index < correosAlmacenados.length; index++) {
+    if (correosAlmacenados[index]==correo.value ) {
+      //&& si contraseñasAlmacenadas es igual a la contresaña.value(login) entonces pasa a página de inicio.
+      
+      console.log("Tiene permiso de entrar al sistema", correosAlmacenados[index]);
+      window.location.href="index.html"
+    } 
+    
+  }
+
+ 
+/*
+  if (correoValido.test(correo.value)) {
+    alert('El correo es valido');
+    return true;
+
+  } else {
+    alert('El correo es invalido');
+    return false;
+  }
+*/  
 }
+
+
+///Tengo que investigar como hacer un objeto///
