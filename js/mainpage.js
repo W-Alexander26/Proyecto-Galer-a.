cc6774fe89974537cc6597122e0fddcaed862ab2
W-Alexander <alexanderaguilar726@gmail.com>
2024-05-10 15:42:22 -0600

    Intento de validaciones

--- js/mainpage.js
+++ js/mainpage.js
@@ -9,20 +9,26 @@ let preVista=document.getElementById('preVista');
 
 /*console.log(searchName);*/
 
-nameUser.innerHTML=(JSON.stringify(changename[0].nombre));
+nameUser.innerHTML=(JSON.stringify(changename[1].nombre));
 //Esto funciona muy bien sin embargo falta mejorarlo*/
 
-function mostraImagen(preVista) {
+function mostrarImagen(event) {
 
-  var file = preVista.files[0];
+  var file = event.target;
+
+  console.log(file);
+
+  let files = file.files;
+
+  let firstFile = files[0];
 
   var reader = new FileReader();
 
-  reader.onloadend = function() {
+  reader.onload= function() {
 
     console.log('RESULT', reader.result)
   }
-  reader.readAsDataURL(file);
+  reader.readAsDataURL(firstFile);
 }
 
 //console.log(changename[0].nombre);//
